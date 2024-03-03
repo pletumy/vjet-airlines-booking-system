b0adb9f4b3be8c765d1c77984e4377ce94d9de6a
Merrick Sakai <ple.tumy@gmail.com>
2024-03-03 15:11:06 +0700

    first commit

---
+++ src/model/SanBay.java
@@ -0,0 +1,35 @@
+package model;
+/**
+ *
+ * @author TuMy
+ */
+public class SanBay {
+
+    private String maSanBay;
+    private String tenSanBay;
+
+    public SanBay() {
+    }
+
+    public SanBay(String maSanBay, String tenSanBay) {
+        this.maSanBay = maSanBay;
+        this.tenSanBay = tenSanBay;
+    }
+
+    public String getMaSanBay() {
+        return maSanBay;
+    }
+
+    public String getTenSanBay() {
+        return tenSanBay;
+    }
+
+    public void setMaSanBay(String maSanBay) {
+        this.maSanBay = maSanBay;
+    }
+
+    public void setTenSanBay(String tenSanBay) {
+        this.tenSanBay = tenSanBay;
+    }
+
+}
